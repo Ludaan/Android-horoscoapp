c647e03fef02be8c52905ea0869c888ddb1e5773
xdaniel <luisdla626@gmail.com>
2024-01-14 21:34:55 -0600

    permissions management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ android {
 dependencies {
 
     val navVersion = "2.7.1"
+    val cameraVersion = "1.2.3"
 
     // navigation component
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
@@ -69,6 +70,12 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.3.1")
 
+    //CameraX
+    implementation("androidx.camera:camera-core:${cameraVersion}")
+    implementation("androidx.camera:camera-camera2:${cameraVersion}")
+    implementation("androidx.camera:camera-lifecycle:${cameraVersion}")
+    implementation("androidx.camera:camera-view:${cameraVersion}")
+    implementation("androidx.camera:camera-extensions:${cameraVersion}")
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
